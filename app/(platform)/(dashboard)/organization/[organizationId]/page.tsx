b8fe5bc27ed089da8dfada99fab7761d2f0c201b
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-12-07 17:16:54 +0300

    Add middleware for url navigation, add custom button variant

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,5 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs";
-
 const OrganizationIdPage = () => {
-  const { userId, orgId } = auth();
-
-  return (
-    <div>
-      <OrganizationSwitcher />
-    </div>
-  );
+  return <div>Organization Page</div>;
 };
 
 export default OrganizationIdPage;
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,39 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // user is logged on and is on a public route
+    if (auth.userId && auth.isPublicRoute) {
+      // user is logged in and has no organization id
+      let path = "/select-org";
+
+      // user is logged in and has organization id, redirect to organization
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    //   user is not logged in and not on public route,
+    // redirect back to origin url
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // user is logged in and has no organization and they try to access
+    // another route other than select-org, redirect them to select-org
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
